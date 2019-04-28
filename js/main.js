9d524de90d29e49a30b5e262a3e19226f379581d
Ellie Milligan <emilliganmaps@gmail.com>
2019-04-27 23:27:45 -0500

    overlay (basemaps, counties, unified districts)

--- js/main.js
+++ js/main.js
@@ -4,66 +4,6 @@ $(document).click(function(){
 
 
 
-var counties = L.layerGroup(Counties);
-
-
-
-//style counties layer
-function style(feature){
-    return {
-        fillColor: "grey",
-        opacity: 0.5,
-        weight: 0.5,
-        color: "grey",
-        fillOpacity: 0.4
-    };
-};
-
-
-
-function createMap(){
-    //create map object
-    var map = L.map("map", {
-        center: [43.7844, -88.7879],
-        zoom: 6,
-        minZoom: 3,
-        maxZoom: 12
-    });
-    
-    L.geoJSON(Counties, {style:style}).addTo(map);
-    
-    
-    //add base tile layer
-    var light = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZW1pbGxpZ2FuIiwiYSI6ImNqczg0NWlxZTBia2U0NG1renZyZDR5YnUifQ.UxV3OqOsN6KuZsclo96yvQ', {
-        //map attribution
-        maxZoom: 18,
-        //mapbox light
-        id: 'mapbox.light',
-        //my unique access token
-        accessToken: 'pk.eyJ1IjoiZW1pbGxpZ2FuIiwiYSI6ImNqczg0NWlxZTBia2U0NG1renZyZDR5YnUifQ.UxV3OqOsN6KuZsclo96yvQ'
-    }),
-        streets = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZW1pbGxpZ2FuIiwiYSI6ImNqczg0NWlxZTBia2U0NG1renZyZDR5YnUifQ.UxV3OqOsN6KuZsclo96yvQ', {
-        //map attribution
-        maxZoom: 18,
-        //mapbox streets
-        id: 'mapbox.streets',
-        //my unique access token
-        accessToken: 'pk.eyJ1IjoiZW1pbGxpZ2FuIiwiYSI6ImNqczg0NWlxZTBia2U0NG1renZyZDR5YnUifQ.UxV3OqOsN6KuZsclo96yvQ'
-    }).addTo(map);
-    
-    getData(map);
-    
-    //add basemaps
-    var baseMaps = {
-        "Greyscale": light,
-        "Streets": streets,
-    };
-    
-    return map;
-};
-
-
-
 //function to retrieve the data and place it on the map
 function getData(map){
     //load the data from the pertussis json
@@ -90,5 +30,95 @@ function getData(map){
 		}
     });
 };
+
+
+
+var counties = L.layerGroup(Counties);
+var districts = L.layerGroup(Districts);
+
+
+
+//style counties layer
+function styleCounties(feature){
+    return {
+        fillColor: "grey",
+        opacity: 0.5,
+        weight: 0.5,
+        color: "black",
+        fillOpacity: 0.4
+    };
+};
+
+
+
+//style districts layer
+function styleDistricts(feature){
+    return {
+        fillColor: "grey",
+        opacity: 0.5,
+        weight: 0.5,
+        color: "black",
+        fillOpacity: 0.4
+    };
+};
+
+
+
+function createMap(){
+    //create map object
+    var map = L.map("map", {
+        center: [44.7844, -88.7879],
+        zoom: 7,
+        minZoom: 3,
+        maxZoom: 12
+    });
+    
+    
+    var counties = new L.geoJSON(Counties, {style:styleCounties}).addTo(map);
+    var districts = new L.geoJSON(Districts, {style:styleDistricts}).addTo(map);
+    
+    
+    getData(map);
+    
+
+    //add base tile layer
+    var light = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZW1pbGxpZ2FuIiwiYSI6ImNqczg0NWlxZTBia2U0NG1renZyZDR5YnUifQ.UxV3OqOsN6KuZsclo96yvQ', {
+        //map attribution
+        maxZoom: 18,
+        //mapbox light
+        id: 'mapbox.light',
+        //my unique access token
+        accessToken: 'pk.eyJ1IjoiZW1pbGxpZ2FuIiwiYSI6ImNqczg0NWlxZTBia2U0NG1renZyZDR5YnUifQ.UxV3OqOsN6KuZsclo96yvQ'
+    }),
+        streets = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZW1pbGxpZ2FuIiwiYSI6ImNqczg0NWlxZTBia2U0NG1renZyZDR5YnUifQ.UxV3OqOsN6KuZsclo96yvQ', {
+        //map attribution
+        maxZoom: 18,
+        //mapbox streets
+        id: 'mapbox.streets',
+        //my unique access token
+        accessToken: 'pk.eyJ1IjoiZW1pbGxpZ2FuIiwiYSI6ImNqczg0NWlxZTBia2U0NG1renZyZDR5YnUifQ.UxV3OqOsN6KuZsclo96yvQ'
+    }).addTo(map);
+    
+    
+    //add basemaps
+    var baseMaps = {
+        "Greyscale": light,
+        "Streets": streets,
+    };
+    
+    //add new data layer
+    var overlayMaps = {
+        "Unified School Districts": districts,
+        "Counties": counties,
+    };
+    
+    //layer control
+    L.control.layers(baseMaps, overlayMaps, {collapsed:false}).addTo(map);
+    
+    return map;
+};
+
+
+
 
 $(document).ready(createMap);
